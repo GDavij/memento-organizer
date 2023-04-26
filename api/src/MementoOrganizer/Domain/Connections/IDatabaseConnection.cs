af7a4245ea1e7498164461ea61380455e512afe6
Gabriel Davi <gdavijac@gmail.com>
2023-04-26 13:23:57 -0300

    Remove Comment Hypotesis

--- api/src/MementoOrganizer/Domain/Connections/IDatabaseConnection.cs
+++ api/src/MementoOrganizer/Domain/Connections/IDatabaseConnection.cs
@@ -1,7 +1,6 @@
 using System;
 namespace MementoOrganizer.Domain.Connections;
 
-// Implement IDisposable to Tests LifeTime of connection till First Homologation Phase
 public interface IDatabaseConnection<TypeDatabase>
 {
     public TypeDatabase Resolve();
