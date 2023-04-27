d7557f24fe20dcb736fa125a41c7d40bfbd95e7e
Gabriel Davi <gdavijac@gmail.com>
2023-04-27 00:57:46 -0300

    Changed Return from Task<bool> to Task

--- api/src/MementoOrganizer/Domain/Repositories/IUsersRepository.cs
+++ api/src/MementoOrganizer/Domain/Repositories/IUsersRepository.cs
@@ -4,7 +4,7 @@ namespace MementoOrganizer.Domain.Repositories;
 
 public interface IUsersRepository<TId>
 {
-    public Task<bool> InsertUser(User<TId> user);
+    public Task InsertUser(User<TId> user);
     public Task<User<TId>> FindUserById(TId id);
     public Task<User<TId>> FindUserByEmail(string email);
     public Task<bool> ExistsAnyAdmin();
