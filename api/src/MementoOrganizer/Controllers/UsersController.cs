a4ba7768410787b6a5c0d9c72392bebd402eab9b
Gabriel Davi <gdavijac@gmail.com>
2023-04-28 15:54:13 -0300

    Setup Users controller

---
+++ api/src/MementoOrganizer/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using MementoOrganizer.Domain.Models.Requests.Users;
+using MementoOrganizer.Domain.Services.Interfaces;
+using MementoOrganizer.Domain.Services;
+using System.Threading.Tasks;
+namespace MementoOrganizer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost]
+    [Route("new/admin")]
+    public async Task<IActionResult> CreateNewAdmin([FromBody()] CreateAdminRequest createAdminRequest)
+    {
+        await _userService.CreateAdmin(createAdminRequest);
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route("new")]
+    public async Task<IActionResult> CreateNewUser([FromBody()] CreateUserRequest createUserRequest)
+    {
+        await _userService.CreateUser(createUserRequest);
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> LoginUser([FromBody()] LoginUserRequest loginUserRequest)
+    {
+        string token = await _userService.LoginUser(loginUserRequest);
+        return Ok(new { token = token });
+    }
+
+    [HttpDelete]
+    [Route("delete/{token}")]
+    public async Task<IActionResult> Deleteuser([FromRoute()] string token)
+    {
+        bool hasBeenDeleted = await _userService.DeleteUser(token);
+        return Ok(hasBeenDeleted);
+    }
+}
