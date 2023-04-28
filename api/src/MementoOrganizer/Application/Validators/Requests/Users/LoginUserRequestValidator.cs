f19e7805d692a48bba6e4e41d2b263d16ba1afa1
Gabriel Davi <gdavijac@gmail.com>
2023-04-28 15:38:44 -0300

    Create Validators for Requests

---
+++ api/src/MementoOrganizer/Application/Validators/Requests/Users/LoginUserRequestValidator.cs
@@ -0,0 +1,17 @@
+using MementoOrganizer.Domain.Models.Requests.Users;
+using FluentValidation;
+namespace MementoOrganizer.Application.Validators.Requests.Users;
+
+public class LoginUserRequestValidator : AbstractValidator<LoginUserRequest>
+{
+    public LoginUserRequestValidator()
+    {
+        RuleFor(req => req.Email)
+            .NotEmpty().WithMessage("E-mail must be send to create a new Admin")
+            .EmailAddress().WithMessage("'email' parameter must be a Valid E-mail");
+
+        RuleFor(req => req.Passphrase)
+            .NotEmpty().WithMessage("Passphrase must be send to create a new Admin")
+            .MinimumLength(16).WithMessage("Passphrase must have a minimum of 16 characters");
+    }
+}
