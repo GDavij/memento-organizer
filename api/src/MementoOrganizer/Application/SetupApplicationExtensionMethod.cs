f8772c91db3041a7a94319f5a3c2c91e32accbe5
Gabriel Davi <gdavijac@gmail.com>
2023-04-28 15:40:35 -0300

    Setup Application Configuration - Extension Method

---
+++ api/src/MementoOrganizer/Application/SetupApplicationExtensionMethod.cs
@@ -0,0 +1,51 @@
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using MementoOrganizer.Domain.Connections;
+using MementoOrganizer.Infrastructure.Connections;
+using MementoOrganizer.Domain.Repositories;
+using MementoOrganizer.Infrastructure.Repositories.Mongo;
+using MongoDB.Bson;
+using MementoOrganizer.Domain.Providers;
+using MementoOrganizer.Infrastructure.Providers;
+using MementoOrganizer.Domain.Services.Interfaces;
+using MementoOrganizer.Domain.Services;
+using MementoOrganizer.Application.Validators.Requests.Users;
+using MementoOrganizer.Domain.Models.Requests.Users;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
+
+namespace MementoOrganizer.Application;
+
+public static class SetupApplicationExtensionMethod
+{
+    public static IServiceCollection SetupInfrastructure(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IDatabaseConnection<IMongoDatabase>, MongoDatabaseConnection>()
+            .AddScoped<IUsersRepository<ObjectId>, MongoUsersRepository>()
+            .AddSingleton<IIdentityProvider<ObjectId>, MongoIdentityProvider>();
+
+        return services;
+    }
+
+    public static IServiceCollection SetupDomain(this IServiceCollection services)
+    {
+        services
+            .AddScoped<ISecurityService, SecurityService>()
+            .AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+
+    public static IServiceCollection SetupApplicationRules(this IServiceCollection services)
+    {
+        services
+            .AddScoped<IValidator<CreateAdminRequest>, CreateAdminRequestValidator>()
+            .AddScoped<IValidator<CreateUserRequest>, CreateUserRequestValidator>()
+            .AddScoped<IValidator<LoginUserRequest>, LoginUserRequestValidator>()
+            .AddFluentValidationAutoValidation();
+
+        return services;
+    }
+}
