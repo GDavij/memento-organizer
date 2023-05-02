16fa27423717bde6277f2dc5552102c8be30f3cc
GDavij <gdavijac@gmail.com>
2023-05-02 16:07:40 -0300

    INotesRepository Definition

---
+++ api/src/MementoOrganizer/Domain/Repositories/INotesRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MementoOrganizer.Domain.Entities;
+namespace MementoOrganizer.Domain.Repositories;
+
+public interface INotesRepository<TId>
+{
+    Task InsertNote(Note<TId> note);
+    Task<List<Note<TId>>> FindAllNotesByOwner(TId ownerId);
+    Task<Note<TId>> FindNoteById(TId id, TId ownerId);
+    Task<bool> ReplaceNoteById(TId id, TId ownerId, Note<TId> note);
+    Task<bool> DeleteNoteById(TId id, TId ownerId);
+}
