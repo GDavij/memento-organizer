1fb40c8bcd8576576d36981bdbb3c703604011a7
GDavij <gdavijac@gmail.com>
2023-05-08 01:40:49 -0300

    Remove BoilerPlate and Setup Basic Design

--- web/memento-organizer-next/next.config.js
+++ web/memento-organizer-next/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    API_URL: process.env["mementoOrganizerApiUrl"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
