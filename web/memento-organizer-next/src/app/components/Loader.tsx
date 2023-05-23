90e74cf65b788489515553fd2955afe9ec4019ec
GDavij <gdavijac@gmail.com>
2023-05-23 01:49:46 -0300

    Feature - Loader Size Configuration

--- web/memento-organizer-next/src/app/components/Loader.tsx
+++ web/memento-organizer-next/src/app/components/Loader.tsx
@@ -1,9 +1,16 @@
 import { MdOutlineSquare } from "react-icons/md";
 
-export default function Loader({ loadingText }: { loadingText: string }) {
+export default function Loader({
+  loadingText,
+  size,
+}: {
+  loadingText: string;
+  size?: string;
+}) {
+  console.log(size);
   return (
     <div className="w-full h-full flex justify-center items-center gap-4 select-none">
-      <span className="animate-spin text-5xl">
+      <span className={`animate-spin ${size ? size : "text-5xl"}`}>
         <MdOutlineSquare />
       </span>
       <span>{loadingText}</span>
