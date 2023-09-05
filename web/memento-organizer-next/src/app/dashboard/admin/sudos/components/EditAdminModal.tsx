dad92286e2434103f3e0e10776e7c42acb71f543
GDavij <gdavijac@gmail.com>
2023-09-05 01:01:36 -0300

    fix: move global components out of app folder

--- web/memento-organizer-next/src/app/dashboard/admin/sudos/components/EditAdminModal.tsx
+++ web/memento-organizer-next/src/app/dashboard/admin/sudos/components/EditAdminModal.tsx
@@ -1,4 +1,4 @@
-import Modal, { BaseModalProps } from '@/app/components/Modal';
+import Modal, { BaseModalProps } from '@/components/Modal';
 import { User } from '@/models/data/user';
 import { useEffect, useId, useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -7,153 +7,153 @@ import usersService from '@/services/user.service';
 import { MdOutlineBackspace } from 'react-icons/md';
 
 type EditSudoModalProps = {
-  admin?: User | undefined;
-  refetchAdmins: () => Promise<void>;
+	admin?: User | undefined;
+	refetchAdmins: () => Promise<void>;
 } & BaseModalProps;
 export function EditUserModal({
-  open,
-  onClose,
-  admin,
-  refetchAdmins,
+	open,
+	onClose,
+	admin,
+	refetchAdmins,
 }: EditSudoModalProps) {
-  const [isUpdatingSudo, setIsUpdatingSudo] = useState(false);
-  type TEditSudoFormData = {
-    email?: string;
-    passphrase?: string;
-    confirmPassphrase?: string;
-  };
+	const [isUpdatingSudo, setIsUpdatingSudo] = useState(false);
+	type TEditSudoFormData = {
+		email?: string;
+		passphrase?: string;
+		confirmPassphrase?: string;
+	};
 
-  const { register, handleSubmit, watch, setValue, reset } =
-    useForm<TEditSudoFormData>({
-      defaultValues: {
-        email: '',
-        passphrase: '',
-        confirmPassphrase: '',
-      },
-    });
+	const { register, handleSubmit, watch, setValue, reset } =
+		useForm<TEditSudoFormData>({
+			defaultValues: {
+				email: '',
+				passphrase: '',
+				confirmPassphrase: '',
+			},
+		});
 
-  useEffect(() => {
-    reset();
-    if (admin) {
-      setValue('email', admin.email);
-    }
-  }, [open]);
+	useEffect(() => {
+		reset();
+		if (admin) {
+			setValue('email', admin.email);
+		}
+	}, [open]);
 
-  async function editUser(formData: TEditSudoFormData) {
-    if (formData.passphrase && formData.passphrase.length < 16) {
-      toast.error('Passphrase has less than 16 characters');
-      return;
-    }
+	async function editUser(formData: TEditSudoFormData) {
+		if (formData.passphrase && formData.passphrase.length < 16) {
+			toast.error('Passphrase has less than 16 characters');
+			return;
+		}
 
-    if (formData.confirmPassphrase && formData.confirmPassphrase.length < 16) {
-      toast.error('Confirm Passphrase has less than 16 characters');
-      return;
-    }
+		if (formData.confirmPassphrase && formData.confirmPassphrase.length < 16) {
+			toast.error('Confirm Passphrase has less than 16 characters');
+			return;
+		}
 
-    if (formData.passphrase != formData.confirmPassphrase) {
-      toast.error('Passphrases missmatch');
-      return;
-    }
+		if (formData.passphrase != formData.confirmPassphrase) {
+			toast.error('Passphrases missmatch');
+			return;
+		}
 
-    let email = formData.email;
-    if (!email || email === admin?.email) email = undefined;
+		let email = formData.email;
+		if (!email || email === admin?.email) email = undefined;
 
-    let passphrase = formData.passphrase;
-    if (!passphrase) passphrase = undefined;
+		let passphrase = formData.passphrase;
+		if (!passphrase) passphrase = undefined;
 
-    setIsUpdatingSudo(true);
-    try {
-      const updateTargetUserRequest = usersService.updateTargetUser(admin!.id, {
-        email,
-        passphrase,
-      });
-      toast.promise(updateTargetUserRequest, {
-        pending: 'Editing Sudo',
-        success: 'Sudo Edited with Sucess',
-        error: 'Could not Edit Sudo',
-      });
-      await updateTargetUserRequest;
-      await refetchAdmins();
-      setIsUpdatingSudo(false);
-      onClose();
-      return;
-    } catch (er) {}
-    setIsUpdatingSudo(false);
-  }
+		setIsUpdatingSudo(true);
+		try {
+			const updateTargetUserRequest = usersService.updateTargetUser(admin!.id, {
+				email,
+				passphrase,
+			});
+			toast.promise(updateTargetUserRequest, {
+				pending: 'Editing Sudo',
+				success: 'Sudo Edited with Sucess',
+				error: 'Could not Edit Sudo',
+			});
+			await updateTargetUserRequest;
+			await refetchAdmins();
+			setIsUpdatingSudo(false);
+			onClose();
+			return;
+		} catch (er) {}
+		setIsUpdatingSudo(false);
+	}
 
-  const emailInputId = useId();
-  const passphraseInputId = useId();
-  const confirmPassphraseInputId = useId();
+	const emailInputId = useId();
+	const passphraseInputId = useId();
+	const confirmPassphraseInputId = useId();
 
-  return (
-    <Modal open={open} onClose={onClose} subject="Update sudo">
-      <form
-        className="flex flex-col gap-4 w-full"
-        onSubmit={handleSubmit(editUser)}
-      >
-        <label htmlFor={emailInputId}>
-          Email
-          <div className="input-wrapper-left-flat">
-            <input
-              {...register('email')}
-              type="email"
-              id={emailInputId}
-              className="w-full h-8 bg-transparent p-6 outline-none"
-            />
-            <span>
-              <button
-                type="button"
-                className="button-flat px-4 py-2"
-                onClick={() => setValue('email', '')}
-              >
-                <MdOutlineBackspace />
-              </button>
-            </span>
-          </div>
-        </label>
-        <label htmlFor={passphraseInputId}>
-          Passphrase
-          <div className="input-wrapper-left-flat">
-            <input
-              {...register('passphrase')}
-              type="password"
-              id={passphraseInputId}
-              className="w-full h-8 bg-transparent p-6 outline-none"
-            />
-            <span>{watch('passphrase')?.length || 0}</span>
-          </div>
-        </label>
+	return (
+		<Modal open={open} onClose={onClose} subject='Update sudo'>
+			<form
+				className='flex flex-col gap-4 w-full'
+				onSubmit={handleSubmit(editUser)}
+			>
+				<label htmlFor={emailInputId}>
+					Email
+					<div className='input-wrapper-left-flat'>
+						<input
+							{...register('email')}
+							type='email'
+							id={emailInputId}
+							className='w-full h-8 bg-transparent p-6 outline-none'
+						/>
+						<span>
+							<button
+								type='button'
+								className='button-flat px-4 py-2'
+								onClick={() => setValue('email', '')}
+							>
+								<MdOutlineBackspace />
+							</button>
+						</span>
+					</div>
+				</label>
+				<label htmlFor={passphraseInputId}>
+					Passphrase
+					<div className='input-wrapper-left-flat'>
+						<input
+							{...register('passphrase')}
+							type='password'
+							id={passphraseInputId}
+							className='w-full h-8 bg-transparent p-6 outline-none'
+						/>
+						<span>{watch('passphrase')?.length || 0}</span>
+					</div>
+				</label>
 
-        <label htmlFor={confirmPassphraseInputId}>
-          Confirm Passphrase
-          <div className="input-wrapper-left-flat">
-            <input
-              {...register('confirmPassphrase')}
-              type="password"
-              id={confirmPassphraseInputId}
-              className="w-full h-8 bg-transparent p-6 outline-none"
-            />
-            <span>{watch('confirmPassphrase')?.length || 0}</span>
-          </div>
-        </label>
+				<label htmlFor={confirmPassphraseInputId}>
+					Confirm Passphrase
+					<div className='input-wrapper-left-flat'>
+						<input
+							{...register('confirmPassphrase')}
+							type='password'
+							id={confirmPassphraseInputId}
+							className='w-full h-8 bg-transparent p-6 outline-none'
+						/>
+						<span>{watch('confirmPassphrase')?.length || 0}</span>
+					</div>
+				</label>
 
-        <div>
-          {(watch('passphrase')?.length || 0) >= 16 &&
-            (watch('confirmPassphrase')?.length || 0) >= 16 &&
-            watch('passphrase') != watch('confirmPassphrase') && (
-              <div className="text-red-400">Passphrases missmatch</div>
-            )}
-          {(watch('passphrase')?.length || 0) >= 16 &&
-            (watch('confirmPassphrase')?.length || 0) >= 16 &&
-            watch('passphrase') === watch('confirmPassphrase') && (
-              <div className="text-green-400">Passphrases match</div>
-            )}
-        </div>
+				<div>
+					{(watch('passphrase')?.length || 0) >= 16 &&
+						(watch('confirmPassphrase')?.length || 0) >= 16 &&
+						watch('passphrase') != watch('confirmPassphrase') && (
+							<div className='text-red-400'>Passphrases missmatch</div>
+						)}
+					{(watch('passphrase')?.length || 0) >= 16 &&
+						(watch('confirmPassphrase')?.length || 0) >= 16 &&
+						watch('passphrase') === watch('confirmPassphrase') && (
+							<div className='text-green-400'>Passphrases match</div>
+						)}
+				</div>
 
-        <button className="button-flat p-4" disabled={isUpdatingSudo}>
-          Edit Sudo
-        </button>
-      </form>
-    </Modal>
-  );
+				<button className='button-flat p-4' disabled={isUpdatingSudo}>
+					Edit Sudo
+				</button>
+			</form>
+		</Modal>
+	);
 }
--- web/memento-organizer-next/src/app/dashboard/components/editor.tsx
+++ web/memento-organizer-next/src/app/dashboard/components/editor.tsx
@@ -1,177 +1,177 @@
 'use client';
 import {
-  Editable,
-  RenderElementProps,
-  RenderLeafProps,
-  Slate,
-  withReact,
+	Editable,
+	RenderElementProps,
+	RenderLeafProps,
+	Slate,
+	withReact,
 } from 'slate-react';
 import { useCallback, useEffect, useMemo } from 'react';
 import {
-  Transforms,
-  createEditor,
-  Descendant,
-  Editor,
-  node,
-  Path,
-  Location,
+	Transforms,
+	createEditor,
+	Descendant,
+	Editor,
+	node,
+	Path,
+	Location,
 } from 'slate';
 
 import { HistoryEditor, withHistory } from 'slate-history';
 import isHotkey from 'is-hotkey';
 import {
-  countOrderedListN,
-  isMarkActive,
-  toggleMark,
+	countOrderedListN,
+	isMarkActive,
+	toggleMark,
 } from '@/lib/editor/editor.aux';
 import { useEditor } from '../contexts/editor/useEditor';
-import Loader from '@/app/components/Loader';
+import Loader from '@/components/Loader';
 import { TBaseNoteData, hotKeys } from '@/models/data/editorTypes';
 import { renderElement, renderLeaf } from '@/lib/editor/markdown.aux';
 import {
-  withImagesFromFiles,
-  withMarkdown,
+	withImagesFromFiles,
+	withMarkdown,
 } from '@/lib/editor/editorExtensions.setup';
 import { useImageStorageCacheContext } from '../contexts/useImageStorageCacheContext';
 import s3ImageStorageService from '@/services/s3ImageStorage.service';
 import { toast } from 'react-toastify';
 
 type TEditorScreenProps = {
-  initialNoteContent: Descendant[];
-  saveNoteCallback: () => Promise<void>;
-  editorId: string;
+	initialNoteContent: Descendant[];
+	saveNoteCallback: () => Promise<void>;
+	editorId: string;
 };
 
 export function EditorScreen({
-  initialNoteContent,
-  saveNoteCallback,
-  editorId,
+	initialNoteContent,
+	saveNoteCallback,
+	editorId,
 }: TEditorScreenProps) {
-  const {
-    setEditor,
-    noteContent,
-    setNoteContent,
-    isBold,
-    setIsBold,
-    isItalic,
-    setIsItalic,
-    isUnderline,
-    setIsUnderline,
-    noteType,
-    setNoteType,
-    isImageLoading,
-    setIsImageLoading,
-  } = useEditor();
+	const {
+		setEditor,
+		noteContent,
+		setNoteContent,
+		isBold,
+		setIsBold,
+		isItalic,
+		setIsItalic,
+		isUnderline,
+		setIsUnderline,
+		noteType,
+		setNoteType,
+		isImageLoading,
+		setIsImageLoading,
+	} = useEditor();
 
-  const editor = useMemo(
-    () =>
-      withHistory(withMarkdown(withImagesFromFiles(withReact(createEditor())))),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
+	const editor = useMemo(
+		() =>
+			withHistory(withMarkdown(withImagesFromFiles(withReact(createEditor())))),
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[]
+	);
 
-  useEffect(() => {
-    setEditor(editor);
-    setNoteContent(initialNoteContent);
-    return () => setNoteContent([]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	useEffect(() => {
+		setEditor(editor);
+		setNoteContent(initialNoteContent);
+		return () => setNoteContent([]);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  return noteContent.length <= 0 ? (
-    <Loader loadingText="Loading Editor" />
-  ) : (
-    <Slate
-      onChange={(value) => {
-        //? Maybe this is not the best implementation of this.
-        // But till now it lead to several performance improvement
-        // This improvement is probaly because of less eventHandlers to work on this component
-        const isBoldMark = isMarkActive(editor, 'bold');
-        const isItalicMark = isMarkActive(editor, 'italic');
-        const isUnderlineMark = isMarkActive(editor, 'underline');
+	return noteContent.length <= 0 ? (
+		<Loader loadingText='Loading Editor' />
+	) : (
+		<Slate
+			onChange={(value) => {
+				//? Maybe this is not the best implementation of this.
+				// But till now it lead to several performance improvement
+				// This improvement is probaly because of less eventHandlers to work on this component
+				const isBoldMark = isMarkActive(editor, 'bold');
+				const isItalicMark = isMarkActive(editor, 'italic');
+				const isUnderlineMark = isMarkActive(editor, 'underline');
 
-        const focusLine = editor.selection?.focus!.path[0] || 0;
-        const dataType = (value as TBaseNoteData[])[focusLine].type;
-        if (noteType != dataType) {
-          setNoteType(dataType);
-        }
+				const focusLine = editor.selection?.focus!.path[0] || 0;
+				const dataType = (value as TBaseNoteData[])[focusLine].type;
+				if (noteType != dataType) {
+					setNoteType(dataType);
+				}
 
-        if (isBold != isBoldMark) {
-          setIsBold(isBoldMark);
-        }
+				if (isBold != isBoldMark) {
+					setIsBold(isBoldMark);
+				}
 
-        if (isItalic != isItalicMark) {
-          setIsItalic(isItalicMark);
-        }
+				if (isItalic != isItalicMark) {
+					setIsItalic(isItalicMark);
+				}
 
-        if (isUnderline != isUnderlineMark) {
-          setIsUnderline(isUnderlineMark);
-        }
-        setNoteContent(value);
-      }}
-      editor={editor}
-      value={noteContent}
-    >
-      <Editable
-        id={editorId}
-        renderElement={(props) => renderElement(props, editor)}
-        renderLeaf={(props) => renderLeaf(props)}
-        placeholder="Start Writing"
-        spellCheck
-        autoFocus
-        className="w-full h-full cursor-text caret-emerald-500 selection:text-emerald-500 selection:bg-slate-200 dark:selection:bg-slate-900"
-        onKeyDown={async (event) => {
-          if (isHotkey('backspace', event as any)) {
-            const focusLine = editor.selection!.focus.path[0];
-            const dataType = (editor.children as TBaseNoteData[])[focusLine]
-              .type;
-            if (dataType != noteType) {
-              setNoteType(dataType);
-            }
-          }
-          if (isHotkey('enter', event as any)) {
-            const focusLine = editor.selection!.focus.path[0];
-            const data = (editor.children as TBaseNoteData[])[focusLine];
-            if (data.type === 'ordered-list') {
-              event.preventDefault();
-              Transforms.insertNodes(editor, {
-                type: 'ordered-list',
-                children: [
-                  { text: `${countOrderedListN(data.children[0].text) + 1}. ` },
-                ],
-              });
-            } else if (data.type === 'unordered-list') {
-              event.preventDefault();
-              Transforms.insertNodes(editor, {
-                type: 'unordered-list',
-                children: [{ text: '- ' }],
-              });
-            }
-          }
-          //TODO: Refactor the hotKeys
-          if (isHotkey('mod+s', event as any)) {
-            event.preventDefault();
-            await saveNoteCallback();
-          } else if (isHotkey('mod+z', event)) {
-            event.preventDefault();
-            editor.undo();
-          } else if (isHotkey('mod+y', event)) {
-            event.preventDefault();
-            editor.redo();
-          } else if (isHotkey('tab', event as any)) {
-            event.preventDefault();
-            Transforms.insertText(editor, '   ');
-          } else {
-            for (const hotkey in hotKeys) {
-              if (isHotkey(hotkey, event as any)) {
-                event.preventDefault();
-                //@ts-ignore
-                const mark = hotKeys[hotkey];
-                toggleMark(editor, mark);
-              }
-            }
-          }
-        }}
-      />
-    </Slate>
-  );
+				if (isUnderline != isUnderlineMark) {
+					setIsUnderline(isUnderlineMark);
+				}
+				setNoteContent(value);
+			}}
+			editor={editor}
+			value={noteContent}
+		>
+			<Editable
+				id={editorId}
+				renderElement={(props) => renderElement(props, editor)}
+				renderLeaf={(props) => renderLeaf(props)}
+				placeholder='Start Writing'
+				spellCheck
+				autoFocus
+				className='w-full h-full cursor-text caret-emerald-500 selection:text-emerald-500 selection:bg-slate-200 dark:selection:bg-slate-900'
+				onKeyDown={async (event) => {
+					if (isHotkey('backspace', event as any)) {
+						const focusLine = editor.selection!.focus.path[0];
+						const dataType = (editor.children as TBaseNoteData[])[focusLine]
+							.type;
+						if (dataType != noteType) {
+							setNoteType(dataType);
+						}
+					}
+					if (isHotkey('enter', event as any)) {
+						const focusLine = editor.selection!.focus.path[0];
+						const data = (editor.children as TBaseNoteData[])[focusLine];
+						if (data.type === 'ordered-list') {
+							event.preventDefault();
+							Transforms.insertNodes(editor, {
+								type: 'ordered-list',
+								children: [
+									{ text: `${countOrderedListN(data.children[0].text) + 1}. ` },
+								],
+							});
+						} else if (data.type === 'unordered-list') {
+							event.preventDefault();
+							Transforms.insertNodes(editor, {
+								type: 'unordered-list',
+								children: [{ text: '- ' }],
+							});
+						}
+					}
+					//TODO: Refactor the hotKeys
+					if (isHotkey('mod+s', event as any)) {
+						event.preventDefault();
+						await saveNoteCallback();
+					} else if (isHotkey('mod+z', event)) {
+						event.preventDefault();
+						editor.undo();
+					} else if (isHotkey('mod+y', event)) {
+						event.preventDefault();
+						editor.redo();
+					} else if (isHotkey('tab', event as any)) {
+						event.preventDefault();
+						Transforms.insertText(editor, '   ');
+					} else {
+						for (const hotkey in hotKeys) {
+							if (isHotkey(hotkey, event as any)) {
+								event.preventDefault();
+								//@ts-ignore
+								const mark = hotKeys[hotkey];
+								toggleMark(editor, mark);
+							}
+						}
+					}
+				}}
+			/>
+		</Slate>
+	);
 }
